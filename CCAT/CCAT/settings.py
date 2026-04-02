2be311641b00dfa49bd098768070e9b14c8d4595
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-02 17:46:09 +0800

    added new files and fixed the logo and tailwind

--- CCAT/CCAT/settings.py
+++ CCAT/CCAT/settings.py
@@ -115,4 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
---
+++ CCAT/ccat_student/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CcatStudentConfig(AppConfig):
+    name = 'ccat_student'
