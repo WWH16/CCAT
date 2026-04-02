9f84d923d5da158fe0f23b639218c73fb1741ffd
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-02 22:55:46 +0800

    add: new file for exam settings

--- CCAT/ccat_admin/urls.py
+++ CCAT/ccat_admin/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('ccat_admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('ccat_admin/question_management/', views.question_management, name='question_management'),
+    path('ccat_admin/exam_settings/', views.exam_settings, name='exam_settings'),
 ]
--- CCAT/ccat_admin/views.py
+++ CCAT/ccat_admin/views.py
@@ -7,3 +7,5 @@ def admin_dashboard(request):
     return render(request, 'ccat_admin/admin_dashboard.html')
 def question_management(request):
     return render(request, 'ccat_admin/question_management.html')
+def exam_settings(request):
+    return render(request, 'ccat_admin/exam_settings.html')
