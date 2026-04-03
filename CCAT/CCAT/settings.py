2ac14296a810814b41d26c4405f811c1f9806485
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-03 16:16:22 +0800

    update: collectstatic logic

--- CCAT/CCAT/settings.py
+++ CCAT/CCAT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y-$4buvr%vnza24)s=2n#h7gxg%_u7g+mlwc-4t*s!(_gi4onq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+
+# This is the folder WHERE collectstatic will put the files
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "CCAT" / "static",
