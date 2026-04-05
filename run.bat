3fcea2028c58a4d1ab594df34f24b3b84c2ce3a7
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-05 11:00:44 +0800

    update on the bat file

--- run.bat
+++ run.bat
@@ -1,5 +1,13 @@
 @echo off
 call venv\Scripts\activate
 cd ccat
+
+:: Open main page in Chrome
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://127.0.0.1:8000/
+
+:: Open student login in Edge
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" http://127.0.0.1:8000/student/login
+
 python manage.py runserver
+
 pause
