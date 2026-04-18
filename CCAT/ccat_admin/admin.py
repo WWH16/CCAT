a989f19c019663bd2a7525a592aa4ae3e8552afd
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-18 19:22:06 +0800

    removed ccat admin models on django admin

--- CCAT/ccat_admin/admin.py
+++ CCAT/ccat_admin/admin.py
@@ -2,17 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Category, Question, Option, Student, ExamResult, SessionKey, ExamConfig
 
 # Customize the admin site headers and titles
 admin.site.site_header = "ISU Palanan CCAT System"
 admin.site.site_title = "ISU Palanan Admin"
 admin.site.index_title = "System Administration"
-
-admin.site.register(Category)
-admin.site.register(Question)
-admin.site.register(Option)
-admin.site.register(Student)
-admin.site.register(ExamResult)
-admin.site.register(SessionKey)
-admin.site.register(ExamConfig)
