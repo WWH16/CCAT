628d91be12948eeee17ebe03ac5bf78966e14199
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-19 15:00:23 +0800

    fix: separated admin base js files

---
+++ CCAT/ccat_admin/static/ccat_admin/js/base.js
@@ -0,0 +1,10 @@
+function toggleLogoutModal(show) {
+    const modal = document.getElementById('logoutModal');
+    if (show) {
+        modal.classList.remove('hidden');
+        document.body.style.overflow = 'hidden';
+    } else {
+        modal.classList.add('hidden');
+        document.body.style.overflow = 'auto';
+    }
+}
