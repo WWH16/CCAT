826cbdb0c1fe69a019ddd48359c5d57f2c261fcc
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-19 12:25:16 +0800

    fix: isu logo on the ccat admin not showing up and admin ccat for examresults

--- CCAT/ccat_admin/admin.py
+++ CCAT/ccat_admin/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from .models import ExamResult, Question, Option, Student, Category
 
 # Customize the admin site headers and titles
 admin.site.site_header = "ISU Palanan CCAT System"
 admin.site.site_title = "ISU Palanan Admin"
 admin.site.index_title = "System Administration"
+
+# admin.site.register(ExamResult)
