075793463db0f8a6eece488e0b087d2e2181993c
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-25 12:59:31 +0800

    remove: admin code

--- CCAT/ccat_admin/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ExamResult
-
-# Only register YOUR custom models
-admin.site.register(ExamResult)
-
-# Do NOT register User - it's already registered by Django
