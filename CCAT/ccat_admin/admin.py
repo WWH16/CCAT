73120e87262e0922e895833ecd3783b6eee58b8d
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-26 13:23:51 +0800

    add: name of the django admin

---
+++ CCAT/ccat_admin/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "CCAT Admin"        # The text at the top of every page
+admin.site.site_title = "CCAT Admin Portal"    # The text in your browser tab
+admin.site.index_title = "Welcome to the CCAT Admin Portal"  # The text at the top of the index page
