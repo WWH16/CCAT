398eca91bd50867dfcb36174f72676df7b2dcc74
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-26 13:36:15 +0800

    refactor: update on the admin py

--- CCAT/ccat_admin/admin.py
+++ CCAT/ccat_admin/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 
-admin.site.site_header = "CCAT Admin"        # The text at the top of every page
-admin.site.site_title = "CCAT Admin Portal"    # The text in your browser tab
-admin.site.index_title = "Welcome to the CCAT Admin Portal"  # The text at the top of the index page
+admin.site.site_header = "CCAT Admin Portal"
+admin.site.index_title = "Welcome to the Computerized College Admission Test (CCAT) Admin Portal"
