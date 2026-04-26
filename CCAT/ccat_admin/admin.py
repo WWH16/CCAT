2842ed25faa923e9513afcc2f8c24e315d276762
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-26 19:17:44 +0800

    remove: groups on the django admin

--- CCAT/ccat_admin/admin.py
+++ CCAT/ccat_admin/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
 
 admin.site.site_header = "CCAT Admin Portal"
 admin.site.index_title = "Welcome to the Computerized College Admission Test (CCAT) Admin Portal"
