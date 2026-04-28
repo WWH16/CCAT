ae90af32e59e66de8c7cfefee2183882cf3fbbd5
John Ansley Rocel <estevesjancen388@gmail.com>
2026-04-28 11:40:29 +0800

    refactor: change computerized into web-based

--- CCAT/ccat_admin/admin.py
+++ CCAT/ccat_admin/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 admin.site.unregister(Group)
 
-admin.site.site_header = "CCAT Admin Portal"
-admin.site.index_title = "Welcome to the Computerized College Admission Test (CCAT) Admin Portal"
+admin.site.site_header = "Super Admin"
+admin.site.index_title = "Welcome to the Web-Based College Admission Test (WCAT)"
